b9af695a27897149aca0bdd1618010001431dfc0
Henrique Martins <henrique.m_s@hotmail.com>
2017-06-20 19:11:53 -0300

    update back here

--- CREATE-TABLEtb_customer_address.sql
+++ CREATE-TABLEtb_customer_address.sql
@@ -5,8 +5,9 @@ street varchar(40),
 lot integer(30),
 refences varchar(40),
 zip_code varchar(40),
+id_customer integer(11),
+cd_address_type char(1),
 
-
- FOREIGN KEY (id_customer),
- FOREIGN KEY (cd_addres_type)
+FOREIGN KEY (id_customer) REFERENCES tb_customer (id_customer),
+FOREIGN KEY (cd_addres_type)REFERENCES dm_adress_type(cd_addres_type)
 );
